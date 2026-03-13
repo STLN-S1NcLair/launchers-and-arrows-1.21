24a4d212eab4c38851e51d7442e8574d96dde953
tabosan <tabosan0916@outlook.jp>
2026-03-13 20:54:17 +0900

    fix: ボルトスロワー一人称視点時の表示

--- src/main/java/net/stln/launchersandarrows/mixin/RenderFirstPersonItemMixin.java
+++ src/main/java/net/stln/launchersandarrows/mixin/RenderFirstPersonItemMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import net.stln.launchersandarrows.item.component.ComponentInit;
 import net.stln.launchersandarrows.item.launcher.BoltThrowerItem;
 import net.stln.launchersandarrows.item.launcher.SlingShotItem;
 import org.spongepowered.asm.mixin.Mixin;
@@ -76,7 +77,7 @@ public class RenderFirstPersonItemMixin {
                 HumanoidArm humanoidarm = flag ? player.getMainArm() : player.getMainArm().getOpposite();
                 poseStack.pushPose();
 
-                boolean flag3 = BoltThrowerItem.isCharged(stack);
+                boolean flag3 = stack.get(ComponentInit.BOLT_COUNT_COMPONENT) != 0;
                 boolean flag2 = humanoidarm == HumanoidArm.RIGHT;
                 int i = flag2 ? 1 : -1;
                 if (player.isUsingItem() && player.getUseItemRemainingTicks() > 0 && player.getUsedItemHand() == hand) {
